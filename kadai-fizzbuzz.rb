8e9b171f29d2aa3188a48b3799f0841f471b6066
yamamoto <omsbiskn26@gmail.jp>
2021-08-02 06:38:18 +0000

    kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,14 +1,14 @@
 num_max = 100
 (1..num_max).each do |number|
 def fizzbuzz number
-   if number % 3 == 0
-    puts "fizz"
+  if number % 3 == 0 && number % 5 == 0 
+      puts "FizzBuzz"
+   elsif number % 3 == 0
+    puts "Fizz"
    elsif number % 5 == 0
-      puts "buzz"
-   elsif number % 15 == 0
-      puts "fizzbuzz"
-    else puts number
-    end
-  end
-  fizzbuzz number
+      puts "Buzz"
+   else puts number
+   end
+end
+fizzbuzz number
 end
