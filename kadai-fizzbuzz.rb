d0fd9827cbb412118d5ae19b6334f782dff6b320
yamamoto <omsbiskn26@gmail.jp>
2021-08-05 08:43:00 +0000

    kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -5,16 +5,11 @@ def fizzbuzz(number)
         puts "Fizz"
     elsif number % 5 == 0
         puts "Buzz"
-    else puts number
+    else 
+      return number
     end
 end
 num_max = 100
 (1..num_max).each do |number|
-    puts fizzbuzz(number) 
+ puts fizzbuzz(number) 
 end
-
-# 以下は動作確認用の記述です
-puts fizzbuzz(1)
-puts fizzbuzz(3)
-puts fizzbuzz(5)
-puts fizzbuzz(15)
