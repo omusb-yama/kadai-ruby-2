e8120e0e48f7eebf0bcbd4d97c66cf00accbc2c1
yamamoto <omsbiskn26@gmail.jp>
2021-08-05 07:48:27 +0000

    kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,14 +1,20 @@
+def fizzbuzz(number)
+    if number % 3 == 0 && number % 5 == 0
+        puts "FizzBuzz"
+    elsif number % 3 == 0
+        puts "Fizz"
+    elsif number % 5 == 0
+        puts "Buzz"
+    else puts number
+    end
+end
 num_max = 100
 (1..num_max).each do |number|
-def fizzbuzz number
-  if number % 3 == 0 && number % 5 == 0 
-      puts "FizzBuzz"
-   elsif number % 3 == 0
-    puts "Fizz"
-   elsif number % 5 == 0
-      puts "Buzz"
-   else puts number
-   end
+    puts fizzbuzz(number) 
 end
-fizzbuzz number
-end
+
+# 以下は動作確認用の記述です
+puts fizzbuzz(1)
+puts fizzbuzz(3)
+puts fizzbuzz(5)
+puts fizzbuzz(15)
